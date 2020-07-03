7995423586c1eed5f901b2b74a07e004db647240
getchipman <chipman@tutanota.com>
2020-07-03 12:03:58 -0300

    modified main for run server

--- main.go
+++ main.go
@@ -3,19 +3,39 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/getchipman/nwapi/models"
 	"github.com/getchipman/nwapi/pkg/setting"
+	"github.com/getchipman/nwapi/routers"
+	"github.com/gin-gonic/gin"
 )
 
-func main() {
+func init() {
 	setting.Setup()
 	models.Setup()
+}
 
-	result, err := models.GetEmployees()
-	if err != nil {
-		log.Fatalln("Erro:", err)
+func main() {
+	gin.SetMode(setting.ServerSetting.RunMode)
+
+	routersInit := routers.InitRouter()
+	readTimeout := setting.ServerSetting.ReadTimeout
+	writeTimeout := setting.ServerSetting.WriteTimeout
+	endPoint := fmt.Sprintf(":%d", setting.ServerSetting.HttpPort)
+	maxHeaderBytes := 1 << 20
+
+	log.Printf("%v", setting.ServerSetting.HttpPort)
+
+	server := &http.Server{
+		Addr:           endPoint,
+		Handler:        routersInit,
+		ReadTimeout:    readTimeout,
+		WriteTimeout:   writeTimeout,
+		MaxHeaderBytes: maxHeaderBytes,
 	}
 
-	fmt.Println(result[0].LastName)
+	log.Printf("[info] start http server listening %s", endPoint)
+
+	server.ListenAndServe()
 }
